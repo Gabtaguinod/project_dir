08a1c6cd042278fc4b9582670b4ae8da334a8b83
GabTaguinod <gabtaguinod09@gmail.com>
2025-11-21 19:05:30 +0800

    second

---
+++ declaring.c
@@ -0,0 +1,15 @@
+//TAGUINOD GABRIEL D. DIT 1-2
+
+#include <stdio.h>
+
+int main(void) {
+
+int age =18;
+float height = 5.3;
+
+printf("Age:  %d\n", age);
+printf("Height: %.1f\n", height);
+
+
+return 0;
+}
